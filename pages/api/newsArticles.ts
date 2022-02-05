deb03d8cf26300fb414a811b0ab1b30ee56ca28d
Megumu Niihara <nhrmgm1223@gmail.com>
2022-02-05 22:41:21 +0900

    add--api news articles

---
+++ pages/api/newsArticles.ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ArticleType } from "../../types/article";
+
+type ArticlesType = {
+  articles: ArticleType[];
+};
+type Error = {
+  error: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ArticlesType | Error>
+) {
+  const pageSize = 20;
+  try {
+    const result = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=jp&pageSize=${pageSize}&apiKey=${process.env.NEWS_API_KEY}`
+    );
+    const json = await result.json();
+    const articles = json.articles;
+    res.status(200).send({ articles });
+  } catch (err) {
+    res.status(500).send({ error: "Failed to retrieve." });
+  }
+}
